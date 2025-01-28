a7f4a7aac272a93c3430b9d47063bcb9b4b74d34
creedthomp <ctt12@byu.edu>
2025-01-28 13:42:27 -0700

    added a few more auth tests

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,14 +1,15 @@
 const request = require('supertest');
 const app = require('../service');
+const { register } = require('module');
 
 const testUser = {name:'pizza diner', email: 'reg@test.com', password: 'a'}
 let testUserAuthToken;
-
+let registerRes;
 
 // Registers a random user every time the test run
 beforeAll(async () => {
     testUser.email = Math.random().toString(36).substring(2,12) + '@test.com';
-    const registerRes = await request(app).post('/api/auth').send(testUser);
+    registerRes = await request(app).post('/api/auth').send(testUser);
     testUserAuthToken = registerRes.body.token;
     expectValidJwt(testUserAuthToken);
 });
@@ -22,6 +23,29 @@ test('login', async() => {
     const expectedUser = {...testUser, roles: [{role: 'diner'}]};
     delete expectedUser.password;
     expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+
+test('Update user', async() => {
+
+    const updateRes = await request(app)
+    .put(`/api/auth/${registerRes.body.user.id}`)
+    .set('Authorization', `Bearer ${testUserAuthToken}`)
+    .send({
+      email: "new@gmail.com",
+      password: 'newpassword',
+    });
+
+
+    expect(updateRes.status).toBe(200);
+    expect(updateRes.body.email).toBe('new@gmail.com');
+});
+
+
+test('logout', async() => {
+    const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(logoutRes.status).toBe(200);
+    expect(logoutRes.body.message).toBe('logout successful');
 });
 
 function expectValidJwt(potentialJwt) {
